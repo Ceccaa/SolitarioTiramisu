1f652917f533d172aeee6cedbbee8d6333b2abe0
MattiaCincotta <146827826+MattiaCincotta@users.noreply.github.com>
2024-05-22 09:42:22 +0200

    fixed buttons

--- SolitarioTiramisù/MainWindow.xaml.cs
+++ SolitarioTiramisù/MainWindow.xaml.cs
@@ -11,6 +11,18 @@ namespace SolitarioTiramisu
             this.WindowState = WindowState.Maximized;
             this.WindowStyle = WindowStyle.None;
             this.ResizeMode = ResizeMode.NoResize;
+
+            // Imposta il percorso del file audio
+            BackgroundMusic.Source = new Uri("pack://application:,,,/background_music.mp3");
+            BackgroundMusic.MediaEnded += BackgroundMusic_MediaEnded; // Riproduci in loop
+            BackgroundMusic.Play();
+        }
+
+        // Metodo per riprodurre la musica in loop
+        private void BackgroundMusic_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            BackgroundMusic.Position = TimeSpan.Zero;
+            BackgroundMusic.Play();
         }
     }
 }
